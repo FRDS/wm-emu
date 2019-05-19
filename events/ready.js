a7aca8efbd9f4d6eacc77410a41325e836713239
FRDS <agung.frds7@gmail.com>
2019-05-19 17:53:41 +0700

    Set Presence

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,12 @@
 module.exports = async (client) => {
-    console.log("Bot is up!");
+    await promisify(res.useServiceAccountAuth)(creds)
+    let info = await promisify(res.getInfo)()
+    let agg = info.worksheets[1];
+    let records = await promisify(agg.getRows)({
+        limit: 4
+    });
+    client.user.setPresence({ game: { name: `with ${records[1].count} records.` }, status: 'idle' });
+    console.log(`Bot is up! ${records[1].count} records and counting...`);
 };
 
 module.exports.help = {
